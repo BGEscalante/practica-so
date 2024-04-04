30c0c9632a322beff7f602ccbc6b1169b3a45d18
Bryan Escalante <42653588+BGEscalante@users.noreply.github.com>
2024-04-04 11:37:03 -0600

    Add files via upload
    cambios en readme y main

---
+++ practica-so/README.md
@@ -0,0 +1,14 @@
+# Practica Sistemas Operativos
+Este repositorio servirá para practicar git en consola
+
+# Estudiantes
+- Arqui
+- Marco Andrés Paz Mazariegos 1528020
+- Pablo
+- Rodrigo Ovalle 1557020
+- Mario
+- Diego Jose Abdo Estrada 1625021
+- María
+- Dennys
+- Josue Manuel Maldonado Lepe  1664220
+- Bryan Gerardo Escalante López 1621817
---
+++ practica-so/main.py
@@ -0,0 +1,26 @@
+print("Este es el curso de Sistemas Operativos")
+print("Docente: Ing. Miguel Matul")
+print("Alumno: Rodrigo Ovalle")
+# Arqui
+
+# Marco
+print("Marco Andrés Paz Mazariegos 1528020")
+
+# Pablo
+
+# Rodrigo
+
+# Mario
+
+# Diego
+print("Diego Jose Abdo Estrada 1625021")
+# María
+
+# Dennys
+
+# Josué
+print("Josue Manuel Maldonado Lepe")
+
+#Bryan
+print("Bryan Gerardo Escalante López 1621817")
+
